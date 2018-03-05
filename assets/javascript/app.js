da9aaebc69bd14e5c4b6faae41f18a33f01cd286
Veronica <chingona27>
2018-03-04 22:33:33 -0600

    2nd commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,35 @@
-// questions that will be used during the game. 
-var questions = [
-    {
-    prompt: "Which presidents served during the '80's? \n(a) Jimmy Carter and George W. Bush \n(b) Bill Clinton Jimmy Carter and George W.H. Bush \n(c) Jimmy Carter George W.H. Bush  Ronald Reagan",
-    answer: "(c)"
-    },
-    {
-    prompt: "Which war ended in the 80's? \n(a) The U.S. is always in war. \n(b) Cold War \n(c) The War on Drugs",
-    answer: "(b)"
-    },
-    {
-    prompt: "Which national holiday became celebrated in the mid-1980's? \n(a) The U.S. has celebrated every national holiday since the beginning of time. \n(b) Labor Day \n(c) Martin Luther King Jr. Day",
-    answer: "(c)"
-    },
-    {
-    prompt: "Who became the first woman to be inducted into the Rock and Roll Hall of Fame? \n(a) Aretha Franklin \n(b) Blondie \n(c) Madonna",
-    answer: "(a)"
-    },]
 
-//score set to 0 3
-var score = 0;
+  
+    // questions that will be used during the game. 
+var questions =
+    ["Which presidents served during the '80's?",
+    "Which war ended in the 80's?",
+    "Which national holiday became celebrated in the mid-1980's?",
+    "Who became the first woman to be inducted into the Rock and Roll Hall of Fame?",
 
-for(var i=0; i < questions.length; i++){
+    ];
+    //logs questions
+    console.log(questions);
+    //answers to questions
+var answerChoices =
+    [
+    ["Jimmy Carter and George W. Bush","Bill Clinton Jimmy Carter and George W.H. Bush","Jimmy Carter George W.H. Bush  Ronald Reagan"],
+    ["The U.S. is always in war","Cold War","The War on Drugs"],
     
-}
-
-//when start button clicked will show timer
-
-//sets time counding down from
-//var countDownTime = new Time("1:00").getTime();
-//var 
+    
+    ];
+    //logs answer choices
+    console.log(answerChoices);
+  
+    //for timer
+var time = 20;    
+       
+    //score set to 0
+var correctAnswers;
+var wrongAnswers;
+var intervalId;
+    
+    /*functions
+    ==========================================================================================================================*/
+    //
+ 
