5340a52a0c30e9d87e43f3f94035aa59f9860499
Veronica <chingona27>
2018-03-05 22:43:37 -0600

    third commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,8 @@
 
-  
+var start;
+var gameHTML;
+    //for timer
+var time = 120;  
     // questions that will be used during the game. 
 var questions =
     ["Which presidents served during the '80's?",
@@ -11,25 +14,56 @@ var questions =
     //logs questions
     console.log(questions);
     //answers to questions
-var answerChoices =
-    [
+var answerChoices = [
     ["Jimmy Carter and George W. Bush","Bill Clinton Jimmy Carter and George W.H. Bush","Jimmy Carter George W.H. Bush  Ronald Reagan"],
     ["The U.S. is always in war","Cold War","The War on Drugs"],
-    
-    
-    ];
+    ["The U.S. has celebrated every national holiday since the beginning of time", "Labor Day","Martin Luther King Jr. Day"],  
+    ["Aretha Franklin, Blondie, Madonna"]
+];
     //logs answer choices
     console.log(answerChoices);
-  
-    //for timer
-var time = 20;    
-       
+var correctAnswers = 
+    ["c. Jimmy Carter George W.H. Bush  Ronald Reagan", 
+    "b. Cold War",
+    "c. Martin Luther King Jr. Day",
+    "a. Aretha Franklin"
+];
+  //logs correct answers
+  console.log(correctAnswers);
+var questionCounter = 0;
+var selectAnswer;
+var theClock;
+var correctTally = 0;
+var incorrectTally = 0;
+var unansweredTally = 0;
     //score set to 0
-var correctAnswers;
+
 var wrongAnswers;
 var intervalId;
     
     /*functions
     ==========================================================================================================================*/
     //
- 
+//start screen 
+$("#StartButton").click(function () {
+    $("#SplashScreen").hide();
+    $("#GameCanvas").show();
+    $("#submitButton").show();
+});
+//start game
+function generateQuiz( ){
+
+    function showQuestions() {
+
+    }
+
+    function showResults() {
+
+    }
+    showQuestions();
+
+    submitButton.onclick = function(){
+        showResults();
+    
+    }
+}
