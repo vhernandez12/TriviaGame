18805591219262d1eeb8aaa7f17dfa9bfe8c043f
Veronica <chingona27>
2018-03-06 18:00:18 -0600

    fourth commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,69 +1,67 @@
 
-var start;
-var gameHTML;
-    //for timer
-var time = 120;  
-    // questions that will be used during the game. 
-var questions =
-    ["Which presidents served during the '80's?",
-    "Which war ended in the 80's?",
-    "Which national holiday became celebrated in the mid-1980's?",
-    "Who became the first woman to be inducted into the Rock and Roll Hall of Fame?",
+  
+    //sumbits quiz
+    function submitQuiz() {
+      console.log('submited');
 
-    ];
-    //logs questions
-    console.log(questions);
-    //answers to questions
-var answerChoices = [
-    ["Jimmy Carter and George W. Bush","Bill Clinton Jimmy Carter and George W.H. Bush","Jimmy Carter George W.H. Bush  Ronald Reagan"],
-    ["The U.S. is always in war","Cold War","The War on Drugs"],
-    ["The U.S. has celebrated every national holiday since the beginning of time", "Labor Day","Martin Luther King Jr. Day"],  
-    ["Aretha Franklin, Blondie, Madonna"]
-];
-    //logs answer choices
-    console.log(answerChoices);
-var correctAnswers = 
-    ["c. Jimmy Carter George W.H. Bush  Ronald Reagan", 
-    "b. Cold War",
-    "c. Martin Luther King Jr. Day",
-    "a. Aretha Franklin"
-];
-  //logs correct answers
-  console.log(correctAnswers);
-var questionCounter = 0;
-var selectAnswer;
-var theClock;
-var correctTally = 0;
-var incorrectTally = 0;
-var unansweredTally = 0;
-    //score set to 0
+    function answerScore (qName) {
+      var radiosNo = document.getElementsByName(qName);
 
-var wrongAnswers;
-var intervalId;
+      for (var i = 0, length  = radiosNo.length; i < length; i++) {
+        if (radiosNo[i.checked]) {
+          var answerValue = Number(radiosNo)[i].value;
+        }
+      }
+      if(isNaN(answerValue)) {
+        answerValue === 0;
+      }
+      return answerValue;
+    }
+      var calcScore = (answerScore('q1') + answerScore('q2') + answerScore('q3') +answerScore('q4'));
+        console.log("calcScore: " + calcScore);
     
-    /*functions
-    ==========================================================================================================================*/
-    //
-//start screen 
-$("#StartButton").click(function () {
-    $("#SplashScreen").hide();
-    $("#GameCanvas").show();
-    $("#submitButton").show();
+    // function to return correct answer string
+		function correctAnswer (correctStringNo, qNumber) {
+			console.log("qNumber: " + qNumber);  // logs 1,2,3,4 after called below
+			return ("The correct answer for question #" + qNumber + ": &nbsp;<strong>" +
+				(document.getElementById(correctStringNo).innerHTML) + "</strong>");
+			}
+
+	// print correct answers only if wrong (calls correctAnswer function)
+		if (answerScore('q1') === 0) {
+			document.getElementById('correctAnswer1').innerHTML = correctAnswer('correctString1', 1);
+		}
+		if (answerScore('q2') === 0) {
+			document.getElementById('correctAnswer2').innerHTML = correctAnswer('correctString2', 2);
+		}
+		if (answerScore('q3') === 0) {
+			document.getElementById('correctAnswer3').innerHTML = correctAnswer('correctString3', 3);
+		}
+		if (answerScore('q4') === 0) {
+			document.getElementById('correctAnswer4').innerHTML = correctAnswer('correctString4', 4);
+		}
+
+	// calculate "possible score" integer
+		var questionCountArray = document.getElementsByClassName('question');
+
+		var questionCounter = 0;
+		for (var i = 0, length = questionCountArray.length; i < length; i++) {
+			questionCounter++;
+		}
+
+	// show score as "score/possible score"
+		var showScore = "Your Score: " + calcScore +"/" + questionCounter;
+	// if 4/4, "perfect score!"
+		if (calcScore === questionCounter) {
+			showScore = showScore + "&nbsp; <strong>Perfect Score!</strong>"
+		};
+		document.getElementById('userScore').innerHTML = showScore;
+	}
+
+$(document).ready(function() {
+
+	$('#submitButton').click(function() {
+		$(this).addClass('hide');
+	});
+
 });
-//start game
-function generateQuiz( ){
-
-    function showQuestions() {
-
-    }
-
-    function showResults() {
-
-    }
-    showQuestions();
-
-    submitButton.onclick = function(){
-        showResults();
-    
-    }
-}
